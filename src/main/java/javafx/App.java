9e44571a9be16bee68852409c7984e5f6daa3a1f
ShocK1999 <przemek.salek@amorki.pl>
2020-11-13 12:50:39 +0100

    login_panel

--- src/main/java/javafx/App.java
+++ src/main/java/javafx/App.java
@@ -1,9 +1,24 @@
 package javafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,15 +29,68 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-
-
+    private GridPane grid;
+    private Text formTitle, notofication;
+    private Label userLogin, userPass;
+    private TextField userLoginField;
+    private PasswordField userPassField;
+    private Button btnLogin;
+    private HBox hbtnLogin;
+    //private String cssPath;               tło można dodać
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+
+        stage.setTitle("Main Panel");
+
+        grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+        //grid.setGridLinesVisible(true);
+
+        formTitle = new Text("Welcome");
+        formTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(formTitle, 0, 0, 2, 1);
+
+        userLogin = new Label("User Name:");
+        grid.add(userLogin, 0, 1);
+
+        userLoginField = new TextField();
+        grid.add(userLoginField, 1, 1);
+
+        userPass = new Label("Password:");
+        grid.add(userPass, 0, 2);
+
+        userPassField = new PasswordField();
+        grid.add(userPassField, 1, 2);
+
+        btnLogin = new Button("Log in");
+        hbtnLogin = new HBox(10);
+        hbtnLogin .setAlignment(Pos.BOTTOM_RIGHT);
+        hbtnLogin .getChildren().add(btnLogin);
+        grid.add(btnLogin, 1, 4);
+        grid.setHalignment(btnLogin, HPos.RIGHT);
+
+        notofication = new Text();
+        grid.add(notofication, 1, 6);
+
+         btnLogin.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                notofication.setFill(Color.FIREBRICK);
+                notofication.setText("Log in button pressed");
+            }
+        });
+
+        //scene = new Scene(loadFXML("primary"),1280, 720);
+        scene = new Scene(grid, 1280, 720);
         stage.setScene(scene);
         stage.show();
     }
+
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
--- src/main/java/javafx/PrimaryController.java
+++ src/main/java/javafx/PrimaryController.java
@@ -2,6 +2,16 @@ package javafx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class PrimaryController {
 
