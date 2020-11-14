55120685563d5bdacc8d803a37a481cb6a5ab7d7
ShocK1999 <przemek.salek@amorki.pl>
2020-11-14 14:02:34 +0100

    better_login_pane

--- src/main/java/javafx/App.java
+++ src/main/java/javafx/App.java
@@ -47,83 +47,10 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        stage.setTitle("Main Panel");
-
-        grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25,25,25,25));           //marginesy
-        //grid.setGridLinesVisible(true);
-
-        formTitle = new Text("Welcome");
-        formTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(formTitle, 0, 0, 2, 1);
-
-        userLogin = new Label("User Name:");
-        grid.add(userLogin, 0, 1);
-
-        userLoginField = new TextField();
-        userLoginField.setPromptText("admin");
-        grid.add(userLoginField, 1, 1);
-
-        userPass = new Label("Password:");
-        grid.add(userPass, 0, 2);
-
-        userPassField = new PasswordField();
-        userPassField.setPromptText("admin");
-        grid.add(userPassField, 1, 2);
-
-        btnLogin = new Button("Log in");
-        hbtnLogin = new HBox(10);
-        hbtnLogin .setAlignment(Pos.BOTTOM_RIGHT);
-        hbtnLogin .getChildren().add(btnLogin);
-        grid.add(btnLogin, 1, 4);
-        //grid.setHalignment(btnLogin, HPos.RIGHT);
-
-        btnReg = new Button("Register on");
-        hbtnReg = new HBox(10);
-        hbtnReg .setAlignment(Pos.BOTTOM_RIGHT);
-        hbtnReg .getChildren().add(btnReg);
-        grid.add(btnReg, 1, 4);
-        grid.setHalignment(btnReg, HPos.RIGHT);
-
-        notofication = new Text();
-        grid.add(notofication, 1, 6);
-
-        btnLogin.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                Connection c = DbConnector.connect();
-                System.out.println(userLoginField.getText() + " " + userPassField.getText());
-                try {
-                    if((userLoginField.getText() != null && !userLoginField.getText().isEmpty() &&
-                            userPassField.getText() != null && !userPassField.getText().isEmpty()) &&
-                            (DbStatements.checkUser(c, userLoginField.getText(), userPassField.getText()) == true)) {
-                        notofication.setFill(Color.GREEN);
-                        notofication.setText("You logged in!");
-                    } else {
-                        notofication.setFill(Color.FIREBRICK);
-                        notofication.setText("Bad login or password!");
-                    }
-                } catch (SQLException ex){
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-        btnReg.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                notofication.setFill(Color.BLUE);
-                notofication.setText("To register, please \ncontact the administrator!");
-            }
-        });
-
-        //scene = new Scene(loadFXML("primary"),1280, 720);
-        image = new Image("https://sm.newsecond.se/2019/08/2019-08-02_14.20.33.png");
-        grid.setBackground(new Background(new BackgroundImage(image,BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
-        scene = new Scene(grid);
+        scene = new Scene(loadFXML("login_pane"));
+//        image = new Image("https://sm.newsecond.se/2019/08/2019-08-02_14.20.33.png");
+//        grid.setBackground(new Background(new BackgroundImage(image,BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
+//        scene = new Scene(grid);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/javafx/LoginController.java
@@ -0,0 +1,48 @@
+package javafx;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import database.DbConnector;
+import database.DbStatements;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class LoginController {
+
+    @FXML
+    private TextField userLoginField;
+
+    @FXML
+    private TextField userPassField;
+
+    @FXML
+    private Text notification;
+
+    @FXML
+    private void log_in() throws IOException {
+        Connection c = DbConnector.connect();
+        System.out.println(userLoginField.getText() + " " + userPassField.getText());
+        try {
+            if((userLoginField.getText() != null && !userLoginField.getText().isEmpty() &&
+                    userPassField.getText() != null && !userPassField.getText().isEmpty()) &&
+                    (DbStatements.checkUser(c, userLoginField.getText(), userPassField.getText()) == true)) {
+                App.setRoot("secondary");
+            } else {
+                notification.setFill(Color.FIREBRICK);
+                notification.setText("Złe hasło lub login!");
+            }
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void reg_on() {
+        notification.setFill(Color.BLUE);
+        notification.setText("Aby się zarejestrować skontaktuj się z administratorem!");
+    }
+}
