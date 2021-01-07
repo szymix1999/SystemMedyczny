71c69368fbd87f0b20f969db8da34ed5f475722c
PituchaAleksander <pitucha.aleksander@gmial.com>
2021-01-07 21:52:47 +0100

    screen_resolution

--- src/main/java/javafx/ApothecaryController.java
+++ src/main/java/javafx/ApothecaryController.java
@@ -17,7 +17,7 @@ public class ApothecaryController {
 
     @FXML
     private TextField SearchField;
-//Testowanie1
+//Testowanie2
     @FXML
     private void SearchAction() throws IOException {
         Connection c = DbConnector.connect();
