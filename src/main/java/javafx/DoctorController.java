dd86aa408aaf9d8b77083d9de8aca5bee6b3fa41
kazdyrkeicam <45139068+kazdyrkeicam@users.noreply.github.com>
2021-01-11 20:48:08 +0100

    doctor_panel

---
+++ src/main/java/javafx/DoctorController.java
@@ -0,0 +1,19 @@
+package javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.awt.event.ActionEvent;
+
+public class DoctorController {
+    /*
+    @FXML private Label label;
+    @FXML private TextField filterField;
+    @FXML private TableView<Patient> tableView;
+    @FXML private TableColumn<Patient, Integer> patID;
+    @FXML private TableColumn<Patient, String> patFirstName;
+    @FXML private TableColumn<Patient, String> patLastName;
+    @FXML private TableColumn<Patient, String> patBirthDate;
+    @FXML private TableColumn<Patient, String> patSex;
+    */
+}
