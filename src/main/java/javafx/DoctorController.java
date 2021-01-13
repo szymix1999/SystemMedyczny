5a1091d02881fa5172ec2451bccc152dc1b8f493
kazdyrkeicam <45139068+kazdyrkeicam@users.noreply.github.com>
2021-01-13 16:00:56 +0100

    doctor_panel

--- src/main/java/javafx/DoctorController.java
+++ src/main/java/javafx/DoctorController.java
@@ -16,4 +16,33 @@ public class DoctorController {
     @FXML private TableColumn<Patient, String> patBirthDate;
     @FXML private TableColumn<Patient, String> patSex;
     */
+
+    /*
+     * CZEGO POTRZEBUJEMY ???
+     * 1. kontakt z baza
+     *  1.1 polaczyc sie z tabela pacjenta
+     *      1.1.1 select z bazy wszystkie info o pacjentach
+     *  1.2 kontakt z lekarzami
+     *      1.2.1 select info o konkretnym lekarzu powiazanym ze skierowaniem
+     *  1.3 kontakt z receptami
+     *  1.4 kontakt ze skierowaniami
+     *  1.5 kontakt z diagnozami
+     *
+     *  CO TO ZNACZY WYBRAC PACJENTA ???
+     *
+     * #if nie wybrany pacjent
+     *  nie mozna dodac diagnozy / skierowania / recepty
+     * #else
+     *  #diagnoza
+     *      tylko nadpisac (dla okreslonego pacjenta)
+     *  #recepta
+     *      zawsze dodajemy (nie nadpisujemy / usuwaja aptekarze)
+     *  #skierowanie
+     *      mozna usunac, po realizacji
+     *      mozna dodac, do konretnego lekarza (trzeba uzyc polaczenia z lekarzami)
+     *
+     * CHYBA TYLE ???
+     */
+
+
 }
